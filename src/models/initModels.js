ebd65dc09b837f5a12e111985ce6b660918b6241
Marcos Plata <marcosplata98@gmail.com>
2022-12-12 23:16:19 -0500

    add quotes controllers and create services

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,10 +1,16 @@
 const Users = require('./users.models')
 const RecoveryPassword = require('./recoveryPasswords.models')
+const Quotes = require('./quotes.models')
 
 const initModels = () => {
     //? FK = RecoveryPassword
     Users.hasMany(RecoveryPassword)
     RecoveryPassword.belongsTo(Users)
+    
+    //? FK = Quotes
+    Quotes.belongsTo(Users)
+    Users.hasMany(Quotes)
+
 }
 
 module.exports = initModels
---
+++ src/quotes/quotes.services.js
@@ -0,0 +1,23 @@
+const quotesControllers = require('./quotes.controllers')
+
+const getAllQuotes = () => {
+    quotesControllers.findAllQuotes()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({message: err.message})
+        })
+}
+
+const postQuote = (req, res) => {
+    const {quote, year} = req.body
+    const userId = req.user.id
+    quotesControllers.createQuotes(obj, {quote, year, userId})
+        .then((data) => {
+            res.status(201).json(data)
+        })
+        .catch(err => {
+            res.status(400).json({message: err.message})
+        })
+}
